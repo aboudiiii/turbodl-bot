07e1b04be49fdace7804a926bb2faa10d86c3096
aboudiiii <abdillhalhayali2005@gmail.com>
2026-08-22 19:47:38 +0300

    Feature: Add fully wired student_hub module

--- bot.py
+++ bot.py
@@ -43,9 +43,9 @@ import downloader
 from downloader import DownloadError, FORMAT_OPTIONS, download_queue
 
 import modules.student_hub
-import modules.media_hub
-import modules.games_hub
-import modules.admin_hub
+# import modules.media_hub
+# import modules.games_hub
+# import modules.admin_hub
 from modules import admin_hub, games_hub
 
 logging.basicConfig(
