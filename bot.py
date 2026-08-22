76579b34658fed5c742d645eefce7130afd23fd0
aboudiiii <abdillhalhayali2005@gmail.com>
2026-08-22 14:50:45 +0300

    Fix admin_hub import binding (from modules import admin_hub)

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ from downloader import DownloadError, FORMAT_OPTIONS, download_queue
 import modules.student_hub
 import modules.media_hub
 import modules.games_hub
-import modules.admin_hub
+from modules import admin_hub
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
